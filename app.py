53875f2ea5cc1ea9e79b056fc72333cae0e08a02
conor <conor.waldron@gmail.com>
2023-12-01 23:33:21 +0000

    add forms to webapp

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' # should be replaced with environment variable later
+
 
 # dummy data to demonstrate Jinja in HTML templates
 title_string = "Conor's Flask App"
@@ -17,12 +20,31 @@ example_blogs = [
 
 @app.route('/')
 @app.route('/home')
-def home_page():
+def home():
     return render_template('home.html', posts=example_blogs, title=title_string)
 
 @app.route('/about')
-def about_page():
+def about():
     return render_template('about.html')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'conor.waldron@optum.com' and form.password.data == '123':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 if __name__ == '__main__':
     app.run(debug=True)
