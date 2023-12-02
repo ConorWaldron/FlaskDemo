058fbf4fa7f15c35b72362a60970143e93f28412
conor <conor.waldron@gmail.com>
2023-12-02 12:49:03 +0000

    add database

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' # should be replaced with environment variable later
+#app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245' # should be replaced with environment variable later
+app.config['SECRET_KEY'] = os.environ.get('FLASK_DEMO_SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+# create database instance
+db = SQLAlchemy(app)
+
+# create class models (tables) that are our database structure
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    image_file = db.Column(db.String(100), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True) # backref
+    def __repr__(self):
+        return f"User({self.username}, {self.email}, {self.image_file})"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # note we dont use () for utcnow as we are passing the function, not the reutned output of the function
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    def __repr__(self):
+        return f"Post({self.title}, {self.date_posted})"
 
 # dummy data to demonstrate Jinja in HTML templates
 title_string = "Conor's Flask App"
